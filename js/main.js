0fb670b2a812720619d424a735716bac444e4f02
kushagharahi <3326002+kushagharahi@users.noreply.github.com>
2023-07-16 16:56:14 -0500

    move to main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,26 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js');
+             .register('./service-worker.js');
   }
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function (e) {
+
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  document.getElementById('divInstall').classList.toggle('hidden', false);
+});
+
+var buttonInstall = document.getElementById('butInstall');
+
+buttonInstall.addEventListener('click', function (e) {
+  // Show the install prompt
+  deferredPrompt.prompt();
+});
--- service-worker.js
+++ service-worker.js
@@ -76,38 +76,3 @@ self.addEventListener("fetch", (event) => {
   // will be handled by the browser as if there were no service
   // worker involvement.
 });
-self.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent the mini-infobar from appearing on mobile.
-  event.preventDefault();
-  console.log('👍', 'beforeinstallprompt', event);
-  // Stash the event so it can be triggered later.
-  self.deferredPrompt = event;
-  // Remove the 'hidden' class from the install button container.
-  divInstall.classList.toggle('hidden', false);
-});
-
-const butInstall = self.document.getElementById('butInstall');
-butInstall.addEventListener('click', async () => {
-  console.log('👍', 'butInstall-clicked');
-  const promptEvent = self.deferredPrompt;
-  if (!promptEvent) {
-    // The deferred prompt isn't available.
-    return;
-  }
-  // Show the install prompt.
-  promptEvent.prompt();
-  // Log the result
-  const result = await promptEvent.userChoice;
-  console.log('👍', 'userChoice', result);
-  // Reset the deferred prompt variable, since
-  // prompt() can only be called once.
-  self.deferredPrompt = null;
-  // Hide the install button.
-  divInstall.classList.toggle('hidden', true);
-});
-
-self.addEventListener('appinstalled', (event) => {
-  console.log('👍', 'appinstalled', event);
-  // Clear the deferredPrompt so it can be garbage collected
-  self.deferredPrompt = null;
-});
