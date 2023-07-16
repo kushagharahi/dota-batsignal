6ddfd9071ee26e191cdfa4c0df2ca9263a92206b
kushagharahi <3326002+kushagharahi@users.noreply.github.com>
2023-07-16 16:22:58 -0500

    add install

--- sw.js
+++ sw.js
@@ -24,3 +24,19 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e) => {
+        deferredPrompt = e;
+    });
+
+    const installApp = document.getElementById('installApp');
+    installApp.addEventListener('click', async () => {
+        if (deferredPrompt !== null) {
+            deferredPrompt.prompt();
+            const { outcome } = await deferredPrompt.userChoice;
+            if (outcome === 'accepted') {
+                deferredPrompt = null;
+            }
+        }
+    });
