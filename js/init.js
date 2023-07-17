ee5c3e1995af7e49d486c57b0c08710b5eef8e46
kushagharahi <3326002+kushagharahi@users.noreply.github.com>
2023-07-17 01:18:17 -0500

    try beams

--- js/init.js
+++ js/init.js
@@ -7,7 +7,7 @@ window.onload = () => {
 }
 window.navigator.serviceWorker.ready.then(serviceWorkerRegistration => {
   const beamsClient = new PusherPushNotifications.Client({
-    instanceId: '57b0db6-6c58-4d3b-bd80-a643f2b32fad',
+    instanceId: '857b0db6-6c58-4d3b-bd80-a643f2b32fad',
     serviceWorkerRegistration: serviceWorkerRegistration,
   })
   beamsClient.start()
