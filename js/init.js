0f6b51035acee33554cfc85d2e838bf884457db3
kushagharahi <3326002+kushagharahi@users.noreply.github.com>
2023-07-17 01:27:21 -0500

    try beams

--- js/init.js
+++ js/init.js
@@ -2,16 +2,15 @@ window.onload = () => {
     'use strict';
   
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./service-worker.js');
+      navigator.serviceWorker.register('./service-worker.js').then((registration) => {
+        const beamsClient = new PusherPushNotifications.Client({
+          instanceId: '857b0db6-6c58-4d3b-bd80-a643f2b32fad',
+          serviceWorkerRegistration: registration,
+        })
+        beamsClient.start()
+        .then(() => beamsClient.addDeviceInterest('hello'))
+        .then(() => console.log('Successfully registered and subscribed!'))
+        .catch(console.error);
+      })
     }
 }
-window.navigator.serviceWorker.ready.then(serviceWorkerRegistration => {
-  const beamsClient = new PusherPushNotifications.Client({
-    instanceId: '857b0db6-6c58-4d3b-bd80-a643f2b32fad',
-    serviceWorkerRegistration: serviceWorkerRegistration,
-  })
-  beamsClient.start()
-  .then(() => beamsClient.addDeviceInterest('hello'))
-  .then(() => console.log('Successfully registered and subscribed!'))
-  .catch(console.error);
-})
