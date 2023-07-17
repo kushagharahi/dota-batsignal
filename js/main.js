042b14b19e15a1d87fbad5e84335fa878290ca0f
kushagharahi <3326002+kushagharahi@users.noreply.github.com>
2023-07-16 21:17:21 -0500

    cleanup

--- js/main.js
+++ js/main.js
@@ -7,23 +7,55 @@ window.onload = () => {
   }
 }
 
+function hideInstallPrompts() {
+  document.getElementById('installContainer').classList.toggle('hidden', true);
+  document.getElementById('iOsInstallContainer').classList.toggle('hidden', true);
+}
+
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', function (e) {
-
-  document.getElementById('installContainer').classList.toggle('hidden', false);
+window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
 
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
+
+  document.getElementById('installContainer').classList.toggle('hidden', false);
+
+  console.log(`'beforeinstallprompt' event was fired.`);
 });
 
 var buttonInstall = document.getElementById('butInstall');
 
-buttonInstall.addEventListener('click', function (e) {
+buttonInstall.addEventListener('click', async () => {
   // Show the install prompt
   deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+  // Optionally, send analytics event with outcome of user choice
+  console.log(`User response to the install prompt: ${outcome}`);
+  // We've used the prompt, and can't use it again, throw it away
+  deferredPrompt = null;
+});
+
+window.addEventListener('appinstalled', () => {
+  // Hide the app-provided install promotion
+  hideInstallPrompts();
+  // Clear the deferredPrompt so it can be garbage collected
+  deferredPrompt = null;
+  // Optionally, send analytics event to indicate successful install
+  console.log('PWA was installed');
+});
+
+window.matchMedia('(display-mode: standalone)').addEventListener('change', (evt) => {
+  let displayMode = 'browser';
+  if (evt.matches) {
+    displayMode = 'standalone';
+    hideInstallPrompts();
+  }
+  // Log display mode change to analytics
+  console.log('DISPLAY_MODE_CHANGED', displayMode);
 });
 
 let isIOS = /iPad|iPhone|iPod/.test(navigator.platform)
